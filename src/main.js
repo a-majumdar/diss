a014c0a288ac3b931644e79e76d7e8fedd47db48
Ananya Majumdar <43969433+a-majumdar@users.noreply.github.com>
2022-10-22 11:15:13 +0100

    scene changes working with slider

--- src/main.js
+++ src/main.js
@@ -1,47 +1,50 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.108.0/build/three.module.js';
 // import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
 
+const container = document.getElementById('scene-container');
+const scene = new THREE.Scene();
+
+const fov = 35;
+const aspect = container.offsetWidth / container.offsetHeight;
+const near = 0.1;
+const far = 100;
+
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far); //maybe use OrthographicCamera for 2d scenes or user interfaces?
+camera.position.set(0,0,10);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(container.clientWidth, container.clientHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+container.append(renderer.domElement);
+
 function main() {
-    const container = document.getElementById('scene-container');
 
-    const scene = new THREE.Scene();
-    // scene.background = new THREE.Color('skyblue');
-
-    const fov = 35;
-    const aspect = container.offsetWidth / container.offsetHeight;
-    const near = 0.1;
-    const far = 100;
-
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far); //maybe use OrthographicCamera for 2d scenes or user interfaces?
-    camera.position.set(0,0,10);
-
-    // const geometry = new THREE.BoxGeometry(2, 2, 2);
-    // const material = new THREE.MeshBasicMaterial();
-    // const cube = new THREE.Mesh(geometry, material);
-    //
-    // scene.add(cube);
-
-    let geometry = new THREE.CircleGeometry( 2, sides );
+    let geometry = new THREE.CircleGeometry( 2, 5 );
     const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
     let circle = new THREE.Mesh( geometry, material );
     scene.add( circle );
-
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(container.clientWidth, container.clientHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
-
-    container.append(renderer.domElement);
-
     renderer.render(scene, camera);
+
 }
 
-function updateCircle(n) {
-    scene.remove(screen.children[0]);
-    let geometry = new THREE.CircleGeometry(2, n);
+main();
+
+var slider = document.getElementById('nSlider');
+slider.oninput = function() {
+    let sides = slider.value;
+    updateScene(sides);
+}
+
+function updateScene(sides) {
+
+    scene.remove.apply(scene, scene.children);
+    // console.log("scene cleared");
+    let geometry = new THREE.CircleGeometry(2, sides);
+    const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
     let circle = new THREE.Mesh(geometry, material);
     scene.add(circle);
+    // console.log("new polygon added");
     renderer.render(scene, camera);
 }
 
-const sides = document.getElementById('nSlider').value;
-main();
