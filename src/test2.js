e79f9b376ba50bc577372ae929ee4c6f5738ee98
Ananya Majumdar <43969433+a-majumdar@users.noreply.github.com>
2022-11-07 15:20:21 +0000

    testing module imports with placeholders

---
+++ src/test2.js
@@ -0,0 +1,10 @@
+class TestClass {
+    constructor() {
+        console.log("this is from test2");
+    }
+
+    message() {
+        return "this is the return string";
+    }
+}
+export {TestClass};
