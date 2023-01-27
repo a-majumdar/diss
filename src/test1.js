d69a5305b0289cab269abe1a1bff975e9796953b
Ananya Majumdar <43969433+a-majumdar@users.noreply.github.com>
2023-01-27 12:01:32 +0000

    leaving fonts for now, moving to other tasks

--- src/test1.js
+++
@@ -1,9 +0,0 @@
-import {TestClass} from './test2.js';
-
-var test2 = new TestClass();
-var h1= document.createElement('H1');
-h1.innerHTML = test2.message();
-console.log(h1.innerHTML);
-
-const container = document.getElementById('testing');
-container.append(h1);
