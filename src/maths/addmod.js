6409ba9dc3412eea37759dd1d2d05fabe6bf746b
s1802817 <s1802817@ed.ac.uk>
2023-02-07 09:50:01 +0000

    refactoring maths classes

--- src/maths/addmod.js
+++ src/maths/addmod.js
@@ -1,83 +1,31 @@
 import * as THREE from 'https://threejs.org/build/three.module.js';
-import { SkeletonHelper } from 'three';
-import { Loop } from '../systems/loop.js';
+// import { SkeletonHelper } from 'three';
+// import { Loop } from '../systems/loop.js';
 // import * as FontLoader from '../three.js-master/examples/js/loaders/FontLoader.js';
 // import { FontLoader } from 'https://threejs.org/examples/jsm/loaders/FontLoader.js';
 // import { TextGeometry } from 'https://threejs.org/examples/jsm/geometries/TextGeometry.js';
 // import * as FontLoader from 'https://threejs.org/examples/jsm/loaders/FontLoader.js';
 // import * as FontLoader from 'fontloader';
 // import {Common} from './common.js';
-import {Screen} from '../screens/screen.js';
+// import {Screen} from '../screens/screen.js';
+import { Modular } from './mod.js';
 
-class Addmod extends Screen {
-
-    size;
-    tail;
-    characters = '0123456789abcdef';
-    charPairs = [];
-    nodes;
-    speed = 1000;
+class Addmod extends Modular {
 
     constructor(c) {
 
         super(c);
-        this.generateCharPairs();
-
-    }
-
-    generateCharPairs() {
-
-        for (let i = 0; i < this.characters.length; i++) {
-            for (let j = 0; j < this.characters.length; j++) {
-                this.charPairs.push(`${this.characters[i] + this.characters[j]}`);
-            }
-        }
-
-    }
-
-    setup() {
-
-        let startN = document.getElementById('nSlider').value;
-        this.updateCircle(startN);
 
     }
 
     updateCircle(sides) {
 
-        this.scene.remove.apply(this.scene, this.scene.children);
-        this.nodes = [];
-        this.tail = [];
-        this.counter = 0;
-        this.size = sides;
-        this.updateLabels();
-        // this.addLabels();
-        document.getElementById('mInverse').innerHTML = "";
-        document.getElementById('stepCount').innerHTML = "";
-
-        let geometry = new THREE.CircleGeometry(2, sides, Math.PI/2);
-        // console.log(geometry);
-        const material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
-        let circle = new THREE.Mesh(geometry, material);
-        // console.log(circle);
-        // this.makeNode(circle, [0,2.1,0]); 
-        // this.nodes[0].material.color.setHex(0xf0ff0f);
-        // console.log(this.nodes[0]);
-        this.indicatorNode();
-
-        for (let i=1;i<=sides;i++) {
-            this.makeNode(circle, geometry.attributes.position.array.slice(3*i, 3*i+3));
-        }
-        let first = this.nodes.pop();
-        this.nodes.unshift(first);
-        this.nodes[this.step].material.color.set('blue');
-        // this.nodes[0].material.color.setHex(`0x00ff00`);
-        // this.loop.updatables.push(this.nodes);
-        this.renderer.render(this.scene, this.camera);
-        // console.log(geometry.vertices);
+        super.updateCircle(sides);
+        this.zeroIndicator();
 
     }
 
-    indicatorNode() {
+    zeroIndicator() {
 
         let direction = new THREE.Vector3(0,-1,0);
         let origin = new THREE.Vector3(0,2.3,0);
@@ -89,17 +37,6 @@ class Addmod extends Screen {
         const indicator = new THREE.ArrowHelper(direction, origin, length, colour, headLength, headWidth);
         this.scene.add(indicator);
 
-    }
-
-    makeNode(parent, vector) {
-        let geometry = new THREE.CircleGeometry(0.05,12);
-        let material = new THREE.MeshBasicMaterial({color:0xf0f0f0});
-        let node = new THREE.Mesh(geometry, material);
-        this.nodes.unshift(node);
-        node.position.set(vector[0], vector[1], vector[2]);
-        node.parent = parent;
-        this.scene.add(node);
-        // console.log(`node ${index} added`);
     }
     
     stepSize(sliderValue) {
@@ -117,23 +54,12 @@ class Addmod extends Screen {
     }
 
     tick() {
-        // console.log(`animating frame ${this.counter}`);
-        let node = this.step * (this.counter + 1);
-        node = node >= this.size ? node % this.size : node;
 
-        if (this.counter < this.size && !this.tail.includes(node)) {
-            this.tail.unshift(node);
-            if (node == 1) { this.mInverse(); }
-            this.changeColours();
-            this.renderer.render(this.scene, this.camera);
-            this.updateLabels();
-            this.sumLabel(node);
-            this.counter += 1;
-            this.renderer.render(this.scene, this.camera);
-        }
-        else {
-            this.loop.stop();
-        }
+        let node = super.tick();
+        if (node == 1) { this.mInverse(); }
+        this.updateLabels();
+        this.sumLabel(node);
+
     }
 
     sumLabel(current) {
@@ -141,8 +67,11 @@ class Addmod extends Screen {
     }
 
     mInverse() {
+
         document.getElementById('mInverse').innerHTML = `The multiplicative inverse of ${this.step} in Z_${this.size} is ${this.counter+1}`;
         this.nodes[1].material.color.setHex('0x0f0f0f');
+        this.renderer.render(this.scene, this.camera);
+
     }
 
     updateLabels() {
@@ -150,21 +79,9 @@ class Addmod extends Screen {
         document.getElementById('iSlider').setAttribute("max", this.size-1);
         document.getElementById('iMax').innerHTML = `The maximum value for i is: ${document.getElementById('iSlider').getAttribute("max")}`;
         document.getElementById('i').innerHTML = `The current step size is ${this.step}`;
-        document.getElementById('counterLabel').innerHTML = `Currently viewing step ${this.counter+1}`;
+        // document.getElementById('counterLabel').innerHTML = `Currently viewing step ${this.counter+1}`;
         document.getElementById('sums').innerHTML = ``;
     }
-
-    changeColours() {
-        for (let i = 0; i < this.tail.length; i++) {
-            let hue = i*25 > 175 ? 'af' : this.charPairs[i*25];
-            this.nodes[this.tail[i]].material.color.setHex(`0xff${hue+hue}`);
-        }
-    }
-
-    // playSpeed(value) {
-    //     this.speed = 3000 / value;
-    // }
-    
 
 }
 
---
+++ src/maths/mod.js
@@ -0,0 +1,116 @@
+import {Screen} from '../screens/screen.js';
+import * as THREE from 'https://threejs.org/build/three.module.js';
+
+
+class Modular extends Screen {
+
+    size;
+    tail;
+    characters = '0123456789abcdef';
+    charPairs = [];
+    nodes;
+    speed = 1000;
+
+    constructor(c) {
+
+        super(c);
+        this.generateCharPairs();
+
+    }
+
+    generateCharPairs() {
+
+        for (let i = 0; i < this.characters.length; i++) {
+            for (let j = 0; j < this.characters.length; j++) {
+                this.charPairs.push(`${this.characters[i] + this.characters[j]}`);
+            }
+        }
+
+    }
+
+    setup() {
+
+        let startN = document.getElementById('nSlider').value;
+        this.updateCircle(startN);
+
+    }
+
+    updateCircle(sides) {
+
+        this.scene.remove.apply(this.scene, this.scene.children);
+        this.nodes = [];
+        this.tail = [];
+        this.counter = 0;
+        this.size = sides;
+        this.updateLabels();
+        // this.addLabels();
+        document.getElementById('mInverse').innerHTML = "";
+        document.getElementById('stepCount').innerHTML = "";
+
+        let geometry = new THREE.CircleGeometry(2, sides, Math.PI/2);
+        // console.log(geometry);
+        const material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
+        let circle = new THREE.Mesh(geometry, material);
+        // console.log(circle);
+        // this.makeNode(circle, [0,2.1,0]); 
+        // this.nodes[0].material.color.setHex(0xf0ff0f);
+        // console.log(this.nodes[0]);
+        // this.indicatorNode();
+
+        for (let i=1;i<=sides;i++) {
+            this.makeNode(circle, geometry.attributes.position.array.slice(3*i, 3*i+3));
+        }
+        let first = this.nodes.pop();
+        this.nodes.unshift(first);
+        this.nodes[this.step].material.color.set('blue');
+        // this.nodes[0].material.color.setHex(`0x00ff00`);
+        // this.loop.updatables.push(this.nodes);
+        this.renderer.render(this.scene, this.camera);
+        // console.log(geometry.vertices);
+
+    }
+
+    makeNode(parent, vector) {
+        let geometry = new THREE.CircleGeometry(0.05,12);
+        let material = new THREE.MeshBasicMaterial({color:0xf0f0f0});
+        let node = new THREE.Mesh(geometry, material);
+        this.nodes.unshift(node);
+        node.position.set(vector[0], vector[1], vector[2]);
+        node.parent = parent;
+        this.scene.add(node);
+        // console.log(`node ${index} added`);
+    }
+
+    tick() {
+        // console.log(`animating frame ${this.counter}`);
+        let node = this.step * (this.counter + 1);
+        node = node >= this.size ? node % this.size : node;
+
+        if (this.counter < this.size && !this.tail.includes(node)) {
+            this.tail.unshift(node);
+            // if (node == 1) { this.mInverse(); }
+            this.changeColours();
+            // this.updateLabels();
+            // this.sumLabel(node);
+            this.counter += 1;
+            this.renderer.render(this.scene, this.camera);
+        }
+        else {
+            this.loop.stop();
+        }
+
+        return node;
+    }
+
+    changeColours() {
+
+        for (let i = 0; i < this.tail.length; i++) {
+            let hue = i*25 > 175 ? 'af' : this.charPairs[i*25];
+            this.nodes[this.tail[i]].material.color.setHex(`0xff${hue+hue}`);
+        }
+    }
+
+
+}
+
+export {Modular};
