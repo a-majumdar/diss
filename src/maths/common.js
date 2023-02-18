db6f1d828c755e11297d48685909759da18be139
s1802817 <s1802817@ed.ac.uk>
2023-02-18 12:17:50 +0000

    tidying

--- src/maths/common.js
+++ src/maths/common.js
@@ -1,13 +1,3 @@
-// function gcd(a, b) {
-//     return !b ? a : gcd(b, a % b);
-// }
-
-// function factors(number) {
-//     return Array.from(Array(number + 1), (_, i) => i).filter(i => number % i === 0);
-// }
-
-
-
 class Common {
 
     gcd(a, b) {
